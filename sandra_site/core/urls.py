dbe8c0def5bad9cc07a8b471e37e445a1205be28
cinbrito <cin.brito@outlook.com>
2026-04-08 17:17:29 -0300

    remoção do projeto Django e adição dos arquivos HTML para o site estático

--- sandra_site/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('galeria/', views.gallery, name='gallery'),
-    path("certificacoes/", views.certifications, name="certifications"),
-    path("servicos/", views.services, name="services"),
-]
